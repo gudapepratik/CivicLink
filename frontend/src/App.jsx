a1fba61ffc7e8fd1be29197b606e810a230a6b56
Pratik <pratikgudape9825@gmail.com>
2025-02-06 17:31:55 +0530

    adding files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div className='h-12 bg-red-200'>
+        <h1>Hello CivicLink !!</h1>
+        <Button></Button>
+      </div>
+    </>
+  )
+}
+
+export default App
